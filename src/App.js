c14ab21ae12b76bb8cefe4f1f23b1bf34dc58ab2
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-14 12:14:19 +1000

    welcome message added to homepage

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HomePage() {
+  return (
+    <h1>Welcome to Freespace</h1>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Welcome to FreeSpace</h1>
+  )
 }
 
 export default App;
