07d499b247f4333ef68346c58a6ac232bc2fbb68
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-14 13:42:03 +1000

    building homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import portfolio from './images/portfolioexample1.png'
 
 function HomePage() {
   return (
-    <h1>Welcome to Freespace</h1>
+    <section>
+      <h1>Welcome to Freespace</h1>
+      <button><h2>Signup to build portfolio</h2></button>
+      <br />
+      <button>Already have an account?</button>  
+    </section>
+  )
+}
+
+function Portfolios() {
+  return (
+    <section id="portfolios">
+      <h3>Browse portfolios</h3>
+      <h4>Portfolios will be appear here</h4>
+      <img src={portfolio} alt="portfolio example 1" />
+      <img src={portfolio} alt="portfolio example 1" />
+      <img src={portfolio} alt="portfolio example 1" />
+      
+    </section>
   )
 }
 
 function App() {
   return (
-    <h1>Welcome to FreeSpace</h1>
+    <>
+      <HomePage />
+      <Portfolios />
+    </>  
   )
 }
 
