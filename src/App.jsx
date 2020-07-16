676f309aeecadb2c7ff34c8e0cff6fdd4685fdb0
Luke <lukelovekin@gmail.com>
2020-07-16 16:32:31 +1000

    some setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import { Route, Switch } from 'react-router-dom'
+// import { Route, Switch } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import { stateReducer, StateContext } from './store'
