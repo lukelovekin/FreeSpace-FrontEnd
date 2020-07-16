f5d624ae4ee81e01477c3d6b7660699127a951c5
Luke <lukelovekin@gmail.com>
2020-07-16 16:05:39 +1000

    some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import api from './api'
 
 function App() {
+  // const [state, dispatch] = useReducer(stateReducer, { portfolios: [] })
+
+  // useEffect(() => {
+  //   api.get('portfolios')
+  //     .then(res => {
+  //       console.log(res)
+  //       // update portfolios in the reducer with the value res.data
+  //       dispatchEvent({
+  //         type: 'setPortfolios',
+  //         data: res.data
+  //       })
+  //     })
+  // }, [])        when this is uncommented and working it should display whats in the portfolio db
+
   return (
     <div className="App">
       <header className="App-header">
