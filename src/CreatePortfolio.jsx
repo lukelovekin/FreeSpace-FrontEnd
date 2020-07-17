2da1496465b5a2ceef6dbfc840c47a5809c31a8f
Luke <lukelovekin@gmail.com>
2020-07-17 10:41:07 +1000

    checking get and post with some routes

--- src/CreatePortfolio.jsx
+++ src/CreatePortfolio.jsx
@@ -1,4 +1,4 @@
-// import React, { useState, useContext } from 'react'
+import React /*, { useState, useContext }*/ from 'react'
 // import { StateContext } from './store'
 // import Axios from 'axios'
 // import api from './api'
