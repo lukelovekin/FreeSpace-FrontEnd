57219b8db98096632466425b99899e674d50e26a
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-20 16:41:59 +1000

    google login button rendering

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login'
 
+
+const responseGoogle = (response) => {
+    console.log(response)
+  }
+
 export default function SignUp() {
     return (
         <div>
             <h1>Sign Up Page</h1>
             <h2>Login with google</h2>
             <GoogleLogin
-            
                 clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
                 buttonText="Login"
                 onSuccess={responseGoogle}
