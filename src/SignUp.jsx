91312fe6bf7db2493b2fbbeb758d660a7d2ad5a9
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-20 16:17:50 +1000

    google oauth tag added, nothing working yet

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import GoogleLogin from 'react-google-login'
 
 export default function SignUp() {
     return (
         <div>
             <h1>Sign Up Page</h1>
             <h2>Login with google</h2>
+            <GoogleLogin
+                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </div>
     )
 }
