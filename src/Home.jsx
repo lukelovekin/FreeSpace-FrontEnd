7d0d929ea5e84e66f309f6d7a4d2d91750a7680e
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-20 16:09:08 +1000

    links from homepage to signup/login pages working

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
             <h1>Welcome to Freespace</h1>
-            <button><h2>Signup to build portfolio</h2></button>
+            <Link to="/sign_up"><button><h2>Sign up to build portfolio</h2></button></Link>
             <br />
-            <button>Already have an account?</button>
+            <Link to="/login"><button>Already have an account?</button></Link>
         </div>
     )
 }
