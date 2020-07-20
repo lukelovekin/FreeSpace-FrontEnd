5b6ea9ffe5688547dde2f10c546d2ef8113d7811
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-20 16:18:58 +1000

    google creditials added, not working though

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -7,6 +7,7 @@ export default function SignUp() {
             <h1>Sign Up Page</h1>
             <h2>Login with google</h2>
             <GoogleLogin
+            
                 clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
                 buttonText="Login"
                 onSuccess={responseGoogle}
