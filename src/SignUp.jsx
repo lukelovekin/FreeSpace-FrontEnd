f610cb946b18a74f1a92e0c8b75f37601968602f
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-20 16:46:11 +1000

    google login client id creditials added

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -12,7 +12,7 @@ export default function SignUp() {
             <h1>Sign Up Page</h1>
             <h2>Login with google</h2>
             <GoogleLogin
-                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                clientId="6695301446-u56bqeioca9b5tl14lki1cgigmsmttk5.apps.googleusercontent.com"
                 buttonText="Login"
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
