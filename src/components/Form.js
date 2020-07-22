49c73a915004d23454958da5ea69d44490847f9c
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-22 13:57:04 +1000

    formatting fixes in form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -34,25 +34,27 @@ class Form extends React.Component {
     }
     render() {
         return (
-        <div className=”form-container”>
-        <form encType=”multipart/form-data” onSubmit={this.onSubmit}> 
-        <h2>Image Form</h2>
-        <label className=”form-label”>Image Title</label>
-        <input 
-        className=”form-input”
-        placeholder=”Enter Image Title”
-        type=”text”
-        value={this.state.title}
-        onChange={this.onChangeTitle}
-        />
-        <label className=”form-label”>Choose an Image</label>
-        <input type=”file” 
-        className=”form-input”
-        onChange={this.onChangeImage} />
-        <button type=”submit” className=”submit-btn”>Submit!</button>
-        </form>
-        </div>
-        );
+            <div className='form-container'>
+                <form encType='multipart/form-data' onSubmit={this.onSubmit}> 
+                    <h2>Image Form</h2>
+                    <label className='form-label'>Image Title</label>
+                    <input 
+                        className='form-input'
+                        placeholder='Enter Image Title'
+                        type='text'
+                        value={this.state.title}
+                        onChange={this.onChangeTitle}
+                    />
+                    <label className='form-label'>Choose an Image</label>
+                    <input 
+                        type='file' 
+                        className='form-input'
+                        onChange={this.onChangeImage} 
+                    />
+                    <button type='submit' className='submit-btn'>Submit!</button>
+                </form>
+            </div>
+        )
     }
 }
 export default connect(null,{ addImage })(Form)
