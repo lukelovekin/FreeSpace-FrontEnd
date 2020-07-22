a903ede26e04ebdf6ef130159c910681c1081d4e
Luke <lukelovekin@gmail.com>
2020-07-22 21:14:18 +1000

    getting oauth working deployed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import Axios from "axios"
 
 export default Axios.create({
-    // baseURL: "https://free-space-api.herokuapp.com/api/"
-    baseURL: "http://localhost:4000/"
+    // baseURL: "http://localhost:4000/"
+    baseURL: "https://free-space-api.herokuapp.com/"
+
 })
