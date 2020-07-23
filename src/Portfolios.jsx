90ffdcb3ae2eb3243afd6a0645051cf424190cc8
Luke <lukelovekin@gmail.com>
2020-07-23 13:27:19 +1000

    moved sign up and added env variables

--- src/Portfolios.jsx
+++ src/Portfolios.jsx
@@ -1,14 +1,30 @@
 import React, { useContext } from 'react'
 import { StateContext } from './store'
 import { Link } from 'react-router-dom'
+import { UserContext } from './store'
 
 export default function Portfolios() {
 const {state /*, dispatch*/} = useContext(StateContext)
+const { user, handleLogOut } = useContext(UserContext)
 
     return (
         <div>
-            <h1>Portfolios</h1>
-            <Link to='/portfolios/new'>New Portfolio</Link>
+            {user ? (
+                    <>
+                        <h1>Welcome to Freespace</h1>
+                        <h2>Logged in as {user.displayName || user.username}</h2>
+                        <Link to='/portfolios/new'><button><h2>Create a Portfolio</h2></button></Link>
+                    </>
+            ) : (
+                    <>
+                        <h1>Welcome to Freespace</h1>
+                        <Link to="/sign_up"><button><h2>Create a Portfolio</h2></button></  Link>
+                        <br />
+                        {/* <Link to="/login"><button>Already   have an account?</button></Link> */}
+                        </>
+            )}
+
+            <h1>Portfolios Below, list or carousel</h1>
             {
                 state.portfolios.map((item, index) => (
                     <article key={index}>
