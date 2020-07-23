fa462eae3c262fde7955fa595468229368aa0965
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-23 10:59:41 +1000

    cloudinary configured

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -9,23 +9,22 @@ const ImageUpload = () => {
   
   const handleImageUpload = () => {
     const { files } = document.querySelector('input[type="file"]')
-    const formData = new FormData();
-    formData.append('file', files[0]);
+    const formData = new FormData()
+    formData.append('file', files[0])
     // replace this with your upload preset name
-    formData.append('upload_preset', 'hpx42bqi');
+    formData.append('upload_preset', 's1eejaxy');
     const options = {
       method: 'POST',
       body: formData,
-    };
+    }
 
-    // replace cloudname with your Cloudinary cloud_name
-    return fetch('https://api.Cloudinary.com/v1_1/highpitchit/image/upload', options)
+    return fetch('https://api.Cloudinary.com/v1_1/dt8aodbvm/image/upload', options)
       .then(res => res.json())
       .then(res => {
         setImageUrl(res.secure_url)
         setImageAlt(`An image of ${res.original_filename}`)
         })
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
   }
 
 
@@ -48,7 +47,7 @@ const ImageUpload = () => {
           )}
         </section>
       </main>
-    );
+    )
   }
 
 
