5735890adf264daeb1aa0070ccc26a78f310c589
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-07-30 06:25:28 +1000

    fixed spelling in about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,13 +13,13 @@ export default function About() {
                     <img src="./location-arrow-solid.svg" alt="" width="50%" />
                 </div>
                 <div className="column">
-                    <h4>FreeSpace is a portfolio sharing app to connect freelancers and profissionals of any kind. Show your work, upload your projects and let the world see what you can do best!</h4>
+                    <h4>FreeSpace is a portfolio sharing app to connect freelancers and professionals of any kind. Show your work, upload your projects and let the world see what you can do best!</h4>
                 </div>
             </div>
             <div className="row"> 
                 <div className="column">
                     <h2>How it works?</h2>
-                    <p className="subtitle">On this page users will learn about Freespace</p>
+                    <p className="subtitle">On this page users will learn about FreeSpace</p>
                 </div>
             </div>
             <div className="row">
@@ -36,7 +36,7 @@ export default function About() {
                 <div className="column">
                     <img src="./location-arrow-solid.svg" alt="" width="50%" />
                     <br /><br /><strong>You can customize your template!</strong>
-                    <p>Make your work look like your work. Feel free to change the things you want to make your portfolio better and pratical for the target audience you want.</p>
+                    <p>Make your work look like your work. Feel free to change the things you want to make your portfolio better and practical for the target audience you want.</p>
                 </div>
             </div>
         </div>
