e3fd2b8b0e92257d2f79b9412300a0f7d42c31ff
Luke <lukelovekin@gmail.com>
2020-08-26 20:11:22 +1000

    create fixed

--- src/components/CreatePortfolio.jsx
+++ src/components/CreatePortfolio.jsx
@@ -188,7 +188,7 @@ export default function CreatePortfolio(props) {
                             <textarea onChange={onChange} value={portfolio.bio} type="text" name="bio" id="bio" />
                 </div>
 
-                {!newPort ? (
+                {newPort ? (
                     <div className="input-group">    
                     <   button  className="btn" onClick={onEdit}>Submit</button>
                     </div>
